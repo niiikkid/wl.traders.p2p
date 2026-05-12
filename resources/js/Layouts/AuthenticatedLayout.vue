da031f12b61d387a970d2ed30906a825a2c0b85f
niiikkid <niiikkid@gmail.com>
2026-05-12 17:38:55 +0700

    Update notification poll interval for improved responsiveness
    - Decreased NOTIFICATION_POLL_INTERVAL_MS from 10000ms to 5000ms in AuthenticatedLayout.vue to increase the frequency of notification checks.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -35,7 +35,7 @@ const notificationTabId = typeof crypto !== 'undefined' && crypto.randomUUID
     : `${Date.now()}-${Math.random().toString(16).slice(2)}`;
 const NOTIFICATION_SOUND_LEADER_TTL_MS = 8000;
 const NOTIFICATION_SOUND_LEADER_HEARTBEAT_MS = 3000;
-const NOTIFICATION_POLL_INTERVAL_MS = 10000;
+const NOTIFICATION_POLL_INTERVAL_MS = 5000;
 
 const syncNotificationSoundSettingsFromProps = () => {
     notificationSoundSettings.value = usePage().props.notificationsSound ?? null;
@@ -224,7 +224,6 @@ const startNotificationsPolling = () => {
     }
 
     startNotificationSoundLeaderHeartbeat();
-    pollNotifications();
     notificationPollInterval.value = setInterval(pollNotifications, NOTIFICATION_POLL_INTERVAL_MS);
 };
 
