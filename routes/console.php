0f1ef5f7135d2f535daaa650948569c3a61ddbb7
niiikkid <niiikkid@gmail.com>
2026-05-14 21:48:26 +0700

    Update Horizon configuration and console schedule for process management
    - Reduced 'maxProcesses' for 'notifications' from 3 to 1 and for 'callback' from 20 to 5 to optimize resource usage.
    - Adjusted the scheduling of 'app:cache-main-page-stats' from every five minutes to every fifteen minutes to reduce load.

--- routes/console.php
+++ routes/console.php
@@ -22,5 +22,5 @@ Schedule::command('app:disconnect-inactive-users')->everyThirtyMinutes();
 
 // Обновление статистики API логов мерчанта каждые 5 минут (включая вчерашний день)
 Schedule::command('api-stats:update')->everyFiveMinutes();
-Schedule::command('app:cache-main-page-stats')->everyFiveMinutes();
+Schedule::command('app:cache-main-page-stats')->everyFifteenMinutes();
 //Schedule::job(new CascadeInternalTimeoutCleanupJob)->everyTenSeconds()->withoutOverlapping();
