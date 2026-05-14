6ce85233056be7875ccb7c5d70f18fdff45cc2af
niiikkid <niiikkid@gmail.com>
2026-05-14 21:28:50 +0700

    Comment out CascadeInternalTimeoutCleanupJob in console routes to prevent execution every ten seconds

--- routes/console.php
+++ routes/console.php
@@ -23,4 +23,4 @@ Schedule::command('app:disconnect-inactive-users')->everyThirtyMinutes();
 // Обновление статистики API логов мерчанта каждые 5 минут (включая вчерашний день)
 Schedule::command('api-stats:update')->everyFiveMinutes();
 Schedule::command('app:cache-main-page-stats')->everyFiveMinutes();
-Schedule::job(new CascadeInternalTimeoutCleanupJob)->everyTenSeconds()->withoutOverlapping();
+//Schedule::job(new CascadeInternalTimeoutCleanupJob)->everyTenSeconds()->withoutOverlapping();
