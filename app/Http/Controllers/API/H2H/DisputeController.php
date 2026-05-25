cace575d399a45fcd86662c93353f985448a161a
niiikkid <niiikkid@gmail.com>
2026-05-25 23:29:33 +0700

    Refactor DisputeController to enhance dispute handling logic
    - Updated `show` and `store` methods to return JsonResponse for better API integration.
    - Added checks for order status in `store` method to prevent disputes on successful or pending orders, providing appropriate failure messages.
    - Improved code readability and maintainability with type hinting and structured response handling.

--- app/Http/Controllers/API/H2H/DisputeController.php
+++ app/Http/Controllers/API/H2H/DisputeController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers\API\H2H;
 
+use App\Enums\OrderStatus;
 use App\Exceptions\DisputeException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\H2H\Dispute\StoreRequest;
 use App\Http\Resources\API\H2H\DisputeResource;
 use App\Models\Order;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Gate;
 
 class DisputeController extends Controller
 {
-    public function show(Order $order)
+    public function show(Order $order): JsonResponse
     {
         if (! $order->is_h2h) {
             return response()->failWithMessage('Сделка предназначена не для H2H API, а для Merchant API.');
@@ -27,13 +29,25 @@ class DisputeController extends Controller
         );
     }
 
-    public function store(StoreRequest $request, Order $order)
+    public function store(StoreRequest $request, Order $order): JsonResponse
     {
         if (! $order->is_h2h) {
             return response()->failWithMessage('Сделка предназначена не для H2H API, а для Merchant API.');
         }
 
         Gate::authorize('access-to-order', $order);
+
+        if ($order->status->equals(OrderStatus::SUCCESS)) {
+            return response()->failWithMessage(
+                "По сделке нельзя открыть спор.\nСделка успешно завершена.\nUUID сделки: {$order->uuid}",
+            );
+        }
+
+        if ($order->status->equals(OrderStatus::PENDING)) {
+            return response()->failWithMessage(
+                "По сделке нельзя открыть спор.\nСделка ещё обрабатывается.\nUUID сделки: {$order->uuid}",
+            );
+        }
 
         try {
             $dispute = services()->dispute()->create($order->id, $request->receipt);
